1f4ad8e42b808fa46173f7d4e2175577babf0ebf
JayMgitM <mowohbaar@yahoo.com>
2023-12-22 20:28:49 -0600

    added message hello world

--- src/main/java/com/baar/countryapp/CountryappApplication.java
+++ src/main/java/com/baar/countryapp/CountryappApplication.java
@@ -1,14 +1,15 @@
 package com.baar.countryapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class CountryappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CountryappApplication.class, args);
-
+		log.info("Country app running here................");
     System.out.println("Hello World!");
 	}
 

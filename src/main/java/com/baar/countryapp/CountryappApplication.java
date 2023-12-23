c1b8ea38af577a46b7638846b224f86126ff98d1
JayMgitM <mowohbaar@yahoo.com>
2023-12-22 20:21:47 -0600

    added message hello world

--- src/main/java/com/baar/countryapp/CountryappApplication.java
+++ src/main/java/com/baar/countryapp/CountryappApplication.java
@@ -8,6 +8,8 @@ public class CountryappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CountryappApplication.class, args);
+
+    System.out.println("Hello World!");
 	}
 
 }

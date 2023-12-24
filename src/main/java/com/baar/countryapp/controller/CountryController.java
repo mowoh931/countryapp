e8a835c469e79e17f2cb705a7e90753159698cdb
JayMgitM <mowohbaar@yahoo.com>
2023-12-24 02:17:08 -0600

    controller updated

--- src/main/java/com/baar/countryapp/controller/CountryController.java
+++ src/main/java/com/baar/countryapp/controller/CountryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@RequestMapping("/api/countries")
+@RequestMapping
 public class CountryController {
 
   private CountryServiceImpl countryServiceImpl;
